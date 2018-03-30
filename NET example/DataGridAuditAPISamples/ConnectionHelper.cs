44116d23d7e795cd8fa3f196cf4519b02b19ab8f
a-yu-relativity <ayu@kcura.com>
2018-03-30 13:24:47 -0500

    Remove old examples

--- NET example/DataGridAuditAPISamples/ConnectionHelper.cs
+++ NET example/DataGridAuditAPISamples/ConnectionHelper.cs
@@ -1,130 +1,159 @@
 ﻿using System;
 using System.Collections.Generic;
-using kCura.Relativity.Client;
-using Relativity.Services.ServiceProxy;
+using System.IO;
 using kCura.AuditUI2.Services.AuditLog;
 using Newtonsoft.Json.Linq;
-using UsernamePasswordCredentials = kCura.Relativity.Client.UsernamePasswordCredentials;
+using Relativity.Services.ServiceProxy;
+
 
 namespace DataGridAuditAPISamples
 {
     class ConnectionHelper
     {
-        //Constants for building the Audit Data Request
-        public const int itemsPerPage = 100000;
-        public const int pageNumber = 1;
-        public const string sortBy = "TimeStamp";
-        public const string sortOrder = "desc";
-        public const bool includeDetails = false;
-        public const bool includeOldNewValues = false;
-        public const string _userName = "YOURRELATIVITYUSERNAME"; /*YOURUSERNAMEHERE*/
-        public const string _password = "YOURRELATIVITYPASSWORD";/*YOURPASSWORDHERE*/
-        public const string BaseRelativityURL = "YOURRELATIVITYURL"; /*EX: https://MySite.relativity.com/Relativity */
-        public const int WorkspaceID = -1;/*TARGET WORKSPACE HERE*/
+        /// <summary>
+        /// Credentials read from a file
+        /// </summary>
+        private string _baseUrl;
+        private string _user;
+        private string _password;
 
-        public static Uri WebApiUri => new Uri(BaseRelativityURL + "webapi/");
-        public static Uri ServicesUri => new Uri(BaseRelativityURL + ".Services");
-        public static Uri RestUri => new Uri(BaseRelativityURL + ".REST/api");
+        private Constants.Enums.AuthType _authType;
 
-        //Method for setting the properties of the Audit Data Request
-        public static void GetValue(AuditLogDataRequest newRequest)
-        {
-            newRequest.SortBy = ConnectionHelper.sortBy;
-            newRequest.SortOrder = ConnectionHelper.sortOrder;
-            newRequest.PageNumber = ConnectionHelper.pageNumber;
-            newRequest.ItemsPerPage = ConnectionHelper.itemsPerPage;
-            newRequest.IncludeDetails = ConnectionHelper.includeDetails;
-            newRequest.IncludeOldNewValues = ConnectionHelper.includeOldNewValues;
-            newRequest.IncludeDetails = ConnectionHelper.includeDetails;
-        }
+        /// <summary>
+        /// Default working workspace artifact ID
+        /// </summary>
+        public static int DefaultWorkspaceId { get; } = 1035745;
 
-        //Method for creating the Audit Data Request
-        public static AuditLogDataRequest AuditLogDataRequest
+        public string BaseRelativityUrl
         {
             get
             {
-                List<int> workspaceIds = new List<int> { -1 };
-                string query = "*";
-                var fields = new List<string>();
-                string filterQuery = ConnectionHelper.BuildFilterQuery(query, fields);
-                AuditLogDataRequest newRequest = new AuditLogDataRequest();
-                newRequest.Workspaces = workspaceIds;
-                newRequest.FilterQuery = filterQuery;
-                GetValue(newRequest);
-                return newRequest;
-            }
-        }
-
-        //Method for generating Audit Data Request Query
-        public static string BuildFilterQuery(string query, IEnumerable<string> fields)
-        {
-            // JSON structure:
-            // { 
-            //   "filtered": { 
-            //     "filter": {},
-            //     "query": {
-            //       "query_string": {
-            //         "query": "<some query>",
-            //         "fields": ["Field1", "Field2"]
-            //       }
-            //     }       
-            //   }
-            // }
-            JObject result = new JObject
-            {
-                ["filtered"] = new JObject
+                string retVal = null;
+                if (!String.IsNullOrEmpty(_baseUrl))
                 {
-                    ["filter"] = new JObject(),
-                    ["query"] = new JObject
+                    // check if URL ends with a slash
+                    if (_baseUrl[_baseUrl.Length - 1].Equals('/'))
                     {
-                        ["query_string"] = new JObject
-                        {
-                            {"query", query},
-                            {"fields", JArray.FromObject(fields)}
-                        }
+                        retVal = _baseUrl + "Relativity";
+                    }
+                    else
+                    {
+                        retVal = _baseUrl + "/Relativity";
                     }
                 }
-            };
-
-            return result.ToString();
+                return retVal;
+            }
         }
 
-        //Instantiates RSAPI proxy
-        public static IRSAPIClient GetRsapiClient()
+        public Uri ServicesUri => new Uri(BaseRelativityUrl + ".Services");
+
+        public Uri RestUri => new Uri(BaseRelativityUrl + ".REST/api");
+
+
+        /// <summary>
+        /// Instantiate with path to credentials file in the format:
+        /// InstanceUrl
+        /// Relativity username
+        /// Relativity password
+        /// </summary>
+        /// <param name="filePath"></param>
+        public ConnectionHelper(string filePath)
         {
             try
             {
-                IRSAPIClient proxy = new RSAPIClient
-                    (ServicesUri, new UsernamePasswordCredentials(_userName, _password));
-                proxy.APIOptions.WorkspaceID = WorkspaceID;
-                return proxy;
+                string[] _creds = File.ReadAllLines(filePath);
+
+                if (_creds.Length == 1)
+                {
+                    Console.WriteLine("Using IntegratedAuth");
+                    _baseUrl = _creds[0];
+                    _authType = Constants.Enums.AuthType.Integrated;
+                }
+                else if (_creds.Length == 3)
+                {
+                    _baseUrl = _creds[0];
+                    _user = _creds[1];
+                    _password = _creds[2];
+                    Console.WriteLine("Using UsernamePassword");
+                    _authType = Constants.Enums.AuthType.UsernamePassword;
+                }
+
+                else
+                {
+                    throw new ApplicationException($"File at {filePath} does not contain the correct number of lines.");
+                }
             }
-            catch (Exception ex)
+
+            catch (IOException)
             {
-                Console.WriteLine("Failed to connect to RSAPI. " + ex);
+                Console.WriteLine("Specified credentials file not found.");
                 throw;
             }
         }
 
-        //Instantiates Services
+
+        public IRSAPIClient GetRsapiClient(int workspaceId = -1)
+        {
+            switch (_authType)
+            {
+                case Constants.Enums.AuthType.Integrated:
+                    return GetRsapiIntegrated(workspaceId);
+
+                case Constants.Enums.AuthType.UsernamePassword:
+                    IRSAPIClient proxy = new RSAPIClient(
+                        ServicesUri,
+                        new UsernamePasswordCredentials(_user, _password));
+                    //proxy.APIOptions.WorkspaceID = DefaultWorkspaceId;
+                    proxy.APIOptions.WorkspaceID = workspaceId;
+                    proxy.Login();
+                    return proxy;
+
+                default:
+                    return GetRsapiIntegrated(workspaceId);
+            }
+        }
+
+
+        /// <summary>
+        /// Gets the RSAPIClient with IntegratedAuth
+        /// </summary>
+        /// <param name="workspaceId"></param>
+        /// <returns></returns>
+        private IRSAPIClient GetRsapiIntegrated(int workspaceId = -1)
+        {
+            var creds = new kCura.Relativity.Client.IntegratedAuthCredentials();
+            IRSAPIClient proxy = new RSAPIClient(
+                ServicesUri,
+                creds);
+            proxy.APIOptions.WorkspaceID = workspaceId;
+            proxy.Login();
+            return proxy;
+        }
+
+
+        /// <summary>
+        /// Gets the service factory with username/password
+        /// </summary>
+        /// <returns></returns>
         public ServiceFactory GetServiceFactory()
         {
-            try
+            Credentials credentials;
+            switch (_authType)
             {
-                Relativity.Services.ServiceProxy.UsernamePasswordCredentials credsService =
-                    new Relativity.Services.ServiceProxy.UsernamePasswordCredentials(_userName, _password);
-                ServiceFactory factory
-                    = new ServiceFactory(new ServiceFactorySettings(ServicesUri, RestUri, credsService));
-                return factory;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-                throw;
+                case Constants.Enums.AuthType.UsernamePassword:
+                    credentials = new Relativity.Services.ServiceProxy.UsernamePasswordCredentials(_user, _password);
+                    break;
+                case Constants.Enums.AuthType.Integrated:
+                    credentials = new Relativity.Services.ServiceProxy.IntegratedAuthCredentials();
+                    break;
+                default:
+                    credentials = new Relativity.Services.ServiceProxy.IntegratedAuthCredentials();
+                    break;
             }
 
+            ServiceFactory factory
+                = new ServiceFactory(new ServiceFactorySettings(ServicesUri, RestUri, credentials));
+            return factory;
         }
-
-
     }
 }
--- NET example/DataGridAuditAPISamples/Program.cs
+++ NET example/DataGridAuditAPISamples/Program.cs
@@ -3,33 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataGridAuditAPISamples.Data_Grid_Audit_Operations;
 
 namespace DataGridAuditAPISamples
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
 
-            Console.WriteLine("##########################################");
-            Console.WriteLine("#       DATAGRID AUDIT API SAMPLES       #");
-            Console.WriteLine("#           ORDER OF SAMPLES:            #");
-            Console.WriteLine("#  1. Find Last Login Date of All Users  #");
-            Console.WriteLine("#  2. Find All Inactive Users (30 days)  #");
-            Console.WriteLine("#  3. Find All Errors in Audit Logs      #");
-            Console.WriteLine("#  4. Find All Actions by Specific User  #");
-            Console.WriteLine("#  5. Find the 100 Most Recent Audits    #");
-            Console.WriteLine("##########################################\r\n");
+            //Console.WriteLine("##########################################");
+            //Console.WriteLine("#       DATAGRID AUDIT API SAMPLES       #");
+            //Console.WriteLine("#           ORDER OF SAMPLES:            #");
+            //Console.WriteLine("#  1. Find Last Login Date of All Users  #");
+            //Console.WriteLine("#  2. Find All Inactive Users (30 days)  #");
+            //Console.WriteLine("#  3. Find All Errors in Audit Logs      #");
+            //Console.WriteLine("#  4. Find All Actions by Specific User  #");
+            //Console.WriteLine("#  5. Find the 100 Most Recent Audits    #");
+            //Console.WriteLine("##########################################\r\n");
             Pause();
-            FindLastLoginAllUsers.findLastLogin();
-            Pause();
-            FindInactiveUsers30Days.findInactives();
-            Pause();
-            FindErrors.findAllErrors();
-            Pause();
-            FindAllActionsByUserName.findAllActions(args[0]);
-            Pause();
-            Top100RecordsAdminLevel.ReturnTop100Audits();
+            //FindLastLoginAllUsers.findLastLogin();
+            //Pause();
+            //FindInactiveUsers30Days.findInactives();
+            //Pause();
+            //FindErrors.findAllErrors();
+            //Pause();
+            //FindAllActionsByUserName.findAllActions(args[0]);
+            //Pause();
+            //Top100RecordsAdminLevel.ReturnTop100Audits();
         }
 
         public static void Pause()
